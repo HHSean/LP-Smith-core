7c56671e9e27bf584ebf2b211a8323d7c9d8beac
김태희 <godtaeheedev@gmail.com>
2022-08-19 10:31:07 +0900

    QuickSwapStrategy Deploy 스크립트 리팩토링

--- scripts/deployQuickSwapStrategy.ts
+++ scripts/deployQuickSwapStrategy.ts
@@ -2,47 +2,45 @@ import { ethers } from "hardhat";
 
 const hre = require("hardhat");
 
-// const me = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-
 const Web3 = require("web3");
+
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const web3 = new Web3(process.env.ALCHEMY_POLYGON_PROVIDER_URL);
-web3.eth.accounts.wallet.add(
-  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-);
 
-const me = web3.eth.accounts.wallet[0];
+const accountZeroPrivateKey =
+  process.env.FORKING_NETWORK_ACCOUNT_ZERO_PRIVATE_KEY;
 
-const approveAmount = web3.utils.toBN(1000000000000000000);
+web3.eth.accounts.wallet.add(accountZeroPrivateKey);
+
+const accountZero = web3.eth.accounts.wallet[0];
 
 async function main() {
   const QuickSwapStrategy = await hre.ethers.getContractFactory(
     "QuickSwapStrategy"
   );
 
-  const DAI_WHALE = "0xd6b26861139a52877Cd7adc437Edd7c5383fF585";
-  const USDC_WHALE = "0xB60C61DBb7456f024f9338c739B02Be68e3F545C";
-  const USDT_WHALE = "0xF977814e90dA44bFA03b6295A0616a897441aceC";
+  const USDC_WHALE_ADDRESS_IN_POLYGON =
+    "0xB60C61DBb7456f024f9338c739B02Be68e3F545C";
+  const USDT_WHALE_ADDRESS_IN_POLYGON =
+    "0xF977814e90dA44bFA03b6295A0616a897441aceC";
 
   const quickSwapStrategy = await QuickSwapStrategy.deploy();
 
-  const contract = await quickSwapStrategy.deployed();
-
   await hre.network.provider.request({
     method: "hardhat_impersonateAccount",
-    params: [USDC_WHALE],
+    params: [USDC_WHALE_ADDRESS_IN_POLYGON],
   });
 
   await hre.network.provider.request({
     method: "hardhat_impersonateAccount",
-    params: [USDT_WHALE],
+    params: [USDT_WHALE_ADDRESS_IN_POLYGON],
   });
 
-  const usdcSigner = await ethers.getSigner(USDC_WHALE);
-  const usdTSigner = await ethers.getSigner(USDT_WHALE);
+  const usdcSigner = await ethers.getSigner(USDC_WHALE_ADDRESS_IN_POLYGON);
+  const usdtSigner = await ethers.getSigner(USDT_WHALE_ADDRESS_IN_POLYGON);
 
   const USDC = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
   const USDT = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F";
@@ -50,27 +48,27 @@ async function main() {
   const usdcContract = await ethers.getContractAt("IERC20", USDC);
   const usdTContract = await ethers.getContractAt("IERC20", USDT);
 
-  const balance = await usdcContract.balanceOf(me.address);
-
-  await usdcContract.connect(usdcSigner).transfer(me.address, 100 * 10 ** 6);
-  await usdTContract.connect(usdTSigner).transfer(me.address, 100 * 10 ** 6);
-
-  const afterBalance = await usdcContract.balanceOf(me.address);
+  await usdcContract
+    .connect(usdcSigner)
+    .transfer(accountZero.address, 100 * 10 ** 6);
+  await usdTContract
+    .connect(usdtSigner)
+    .transfer(accountZero.address, 100 * 10 ** 6);
 
   await usdcContract.approve(quickSwapStrategy.address, 1000 * 10 ** 6);
   await usdTContract.approve(quickSwapStrategy.address, 1000 * 10 ** 6);
 
-  const result = await contract.addLiquidity(
+  const addLiquidityResult = await quickSwapStrategy.addLiquidity(
     USDT,
     USDC,
     10 * 10 ** 6,
     10 * 10 ** 6
   );
 
-  console.log("result");
-  console.log(result);
-
   console.log("QuickSwapStrategy deployed to:", quickSwapStrategy.address);
+
+  console.log("addLiquidityResult");
+  console.log(addLiquidityResult);
 }
 
 main()
